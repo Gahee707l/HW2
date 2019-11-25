8ae7e924dc95f540048fd9be3988d0462584d89c
Gahee707l <gahee707l@gmail.com>
2019-11-25 18:22:40 +0900

    20191125.answ

--- src/PrimeNo.java
+++ src/PrimeNo.java
@@ -25,6 +25,8 @@ public class PrimeNo {
         for (char c : ch) {
             System.out.print(c + " ");
         }
+       // System.out.println(new String(ch));
+        //이렇게도 생성 됨
         System.out.println();
          
         ch = i2.toCharArray();
@@ -42,9 +44,17 @@ public class PrimeNo {
         int j;
  
         for (int i = 2; i <= 100; i++) {
+        	// int count =0;
             for (j = 2; j <= i; j++) {
+            	//		j<i,자기 자신 뺀거
+            	//j=1;	j<=i ;j++
  
                 if (i % j == 0 || i % temp == 0) {
+                	//if(i%j==0)
+                	//count++;
+                	//하나라도 나눠지는게 있었다는 얘기임
+                	//j를 1부터 시작하면  count가 2로 해야되기 때문에 break를 붙일수도 없고 끝까지 비교한다.
+          
                     // :소수를 저장하는 다른분 알고리즘 아이디어 듣고 간식으로. (i != 2) &&
                     break;
                 }
@@ -52,6 +62,7 @@ public class PrimeNo {
             if (i == j) {
                 temp = i;
                 // 아래에 두니...바로 탈출해서 출력이 안돼서 그냥 j 바깥으로 뺌
+              //if (count==0)
                 System.out.print(i + " ");
             }
  
